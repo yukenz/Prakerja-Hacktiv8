5edbec8d859baa46745c722aa412f6aa061a8c76
yukenz <yuyun.purniawan@gmail.com>
2023-10-13 14:53:18 +0700

    Initial Upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const menu = document.getElementById("menu");
+
+// <li><a href="#about">About</a></li>
+// <li><a href="#pengalaman">Experience</a></li>
+// <li><a href="#pendidikan">Education</a></li>
+// <li><a href="#kemampuan">Skill</a></li>
+// <li><a href="#ketertarikan">Interest</a></li>
+
+function listLinkBuilder(innerTag, link = "#") {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.innerHTML = innerTag
+    a.href = link
+    li.appendChild(a)
+    return li
+}
+
+const ul = document.createElement("ul");
+
+// <!--                <li><a href="#about">About</a></li>-->
+// <!--                <li><a href="#pengalaman">Experience</a></li>-->
+// <!--                <li><a href="#pendidikan">Education</a></li>-->
+// <!--                <li><a href="#kemampuan">Skill</a></li>-->
+// <!--                <li><a href="#ketertarikan">Interest</a></li>-->
+
+ul.appendChild(listLinkBuilder("About", "#about"))
+ul.appendChild(listLinkBuilder("Experience", "#pengalaman"))
+ul.appendChild(listLinkBuilder("Education", "#pendidikan"))
+ul.appendChild(listLinkBuilder("Interest", "#ketertarikan"))
+
+menu.appendChild(ul)
